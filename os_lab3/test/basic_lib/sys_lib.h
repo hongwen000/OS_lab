8ce40404bf87363536e6b9fbf52548099e0e325b
李新锐 <hongwen000@outlook.com>
2018-03-25 03:54:05 +0800

    上一次没上传完

--- os_lab3/test/basic_lib/sys_lib.h
+++ os_lab3/test/basic_lib/sys_lib.h
@@ -9,12 +9,17 @@ extern "C"{
     void sys_bios_clear_screen();
     void sys_bios_putchar(char c, int color, int x, int y);
     int  sys_bios_getchar();
-
     uint8_t sys_inb(uint16_t port);
-
     char sys_get_scancode();
     int  sys_getchar();
     void sys_putchar(char c, int color, int x, int y);
+    int  sys_tty_get_x();
+    int  sys_tty_get_y();
+    int  sys_tty_get_color();
+    void sys_tty_set_x(int x);
+    void sys_tty_set_y(int y);
+    void sys_tty_set_color(int color);
+    void sys_tty_putchar(char c);
 
 #ifdef __cplusplus
 }
