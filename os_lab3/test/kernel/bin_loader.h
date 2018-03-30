2ceded4865d9ed6a1731dcec72385b2b9fc7dc80
李新锐 <hongwen000@outlook.com>
2018-03-31 01:52:31 +0800

    成功调整了子程序的段地址

--- os_lab3/test/kernel/bin_loader.h
+++ os_lab3/test/kernel/bin_loader.h
@@ -13,12 +13,13 @@ class bin_loader
 public:
     static void load_binary_from_floppy()
     {
-        sys_read_disk(user_prog_load_addr, 32, 2);
+        sys_read_disk(user_prog_segment, user_prog_load_addr, 32, 2);
         sys_execve_bin();
     }
 
 private:
     static constexpr uint32_t user_prog_load_addr = 0xA100;
+    static constexpr uint32_t user_prog_segment = 0x1000;
 };
 
 #endif //TEST_BIN_LOADER_H
