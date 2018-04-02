790d50e02352ab0d2d1bfa2f06d239ab3385b430
李新锐 <hongwen000@outlook.com>
2018-04-02 12:07:10 +0800

    完成两个系统调用

--- os_lab3/test/libc/sys/hhos.h
+++ os_lab3/test/libc/sys/hhos.h
@@ -3,4 +3,34 @@
 #include "../../kernel/tty.h"
 #include "../../basic_lib/sys_lib.h"
 tty* sys_get_current_tty();
+
+static inline int system_call_getchar()
+{
+    int ret = 0;
+    asm volatile (
+    "movb $0, %%ah\n\t"
+    "int $0x99\n\t"
+    "movl %%eax, %0\n\t"
+    :"=r"(ret)
+    :
+    :"%eax"
+    );
+    int mask = 0x00FF;
+    ret = ret & mask;
+    return ret;
+}
+static inline void system_call_putchar(int ch)
+{
+    int mask = 0x0FF;
+    ch = ch & mask;
+    char c = ch;
+    asm volatile(
+    "movb %0, %%al\n\t"
+    "movb $1, %%ah\n\t"
+    "int $0x98\n\t"
+    :
+    :"r"(c)
+    :"%eax"
+    );
+}
 #endif
