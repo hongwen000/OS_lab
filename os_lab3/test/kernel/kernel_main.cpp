c36b7ad29389f0f2b217b092ff91c64639d74ef7
李新锐 <hongwen000@outlook.com>
2018-04-09 10:04:57 +0800

    实验四可以交了

--- os_lab3/test/kernel/kernel_main.cpp
+++ os_lab3/test/kernel/kernel_main.cpp
@@ -3,6 +3,7 @@
 #include "../basic_lib/sys_lib.h"
 #include "../libc/string.h"
 #include "../libc/stdio.h"
+#include "../libc/sys/hhos.h"
 #include "sh.h"
 static tty* current_tty = nullptr;
 tty* sys_get_current_tty(){return current_tty;}
@@ -22,8 +23,6 @@ static inline void print_ok(char * mod) {
 }
 extern "C" void kernel_main()
 {
-
-
     tty tty1;
     current_tty = &tty1;
     print_ok("TTY");
