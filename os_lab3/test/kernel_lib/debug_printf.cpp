ccf3c50934a8a8af3c1d37ae5cc03dae7b0a75b1
李新锐 <hongwen000@outlook.com>
2018-04-26 01:25:36 +0800

    不分离堆栈了，可以跑

--- os_lab3/test/kernel_lib/debug_printf.cpp
+++ os_lab3/test/kernel_lib/debug_printf.cpp
@@ -8,45 +8,22 @@
 #include "../libc/stdlib.h"
 #include "../libc/ctype.h"
 
-static char buf[100];
-template <typename T>
-T my_va_arg(va_list ap)
-{
-
-    ap += sizeof(T);
-
-    T* addr = (T *)(ap - (sizeof(T)));
-
-
-    T ret;
-
-    asm volatile(
-    "movl %%ss:(%1), %0"
-    :"=r"(ret)
-    :"r"(addr)
-    );
-
-
-
-    return ret;
-//    bochs_break();
-}
-int debug_putchar( int ch )
+static int putchar( int ch )
 {
     sys_dbg_bochs_putc(ch);
     return ch;
 }
 
-int debug_puts(const char* string)
+static int puts(const char* string)
 {
     auto len = strlen(string);
     for(size_t i = 0; i < len; ++i)
     {
-        debug_putchar(*(string + i));
+        putchar(*(string + i));
     }
     return 0;
 }
-int debug_vsprintf( char* buffer, const char* format, va_list vlist )
+static int vsprintf( char* buffer, const char* format, va_list vlist )
 {
     auto len_fmt = strlen(format);
     auto p_fmt = format;
@@ -63,7 +40,6 @@ int debug_vsprintf( char* buffer, const char* format, va_list vlist )
             //此时p_fmt = '%'
             char specifier = *(++p_fmt); //%后面一个字符是specifier
             ++p_fmt;                     //记录specifier并跳过
-
             switch (specifier)
             {
                 case 'c':
@@ -75,19 +51,12 @@ int debug_vsprintf( char* buffer, const char* format, va_list vlist )
                      to a function taking a variable number of arguments,
                      it will be converted to int
                     */
-//                    *(p_buf++) = va_arg(vlist, int);
-                    *(p_buf++) = my_va_arg<int>(vlist);
+                    *(p_buf++) = va_arg(vlist, int);
                     break;
                 }
                 case 's':
                 {
-                    char * str = my_va_arg<char *>(vlist);
-                    vlist+= sizeof(char*);
-////                    char* arg_in_stack = va_arg(vlist, char*);
-//                    debug_puts("In vsprintf, %s got ");
-//                    debug_puts(str);
-//                    debug_puts("\n");
-//                    bochs_break();
+                    const char * str = va_arg(vlist, char*);
                     strcpy(p_buf, str);
                     p_buf += strlen(str);               //p应当永远指向有意义的字符后面一位
                     break;
@@ -95,45 +64,35 @@ int debug_vsprintf( char* buffer, const char* format, va_list vlist )
                 case 'd':
                 case 'i':
                 {
-//                    int num = va_arg(vlist, int);
-                    int num = my_va_arg<int>(vlist);
-                    vlist+= sizeof(int);
+                    int num = va_arg(vlist, int);
                     itoa(p_buf, num, 10);
                     p_buf += strlen(p_buf);             //同理
                     break;
                 }
                 case 'o':
                 {
-//                    unsigned int num = va_arg(vlist, int);
-                    unsigned int num = my_va_arg<unsigned int>(vlist);
-                    vlist+= sizeof(unsigned int);
+                    unsigned int num = va_arg(vlist, int);
                     itoa(p_buf, num, 8);
                     p_buf += strlen(p_buf);             //同理
                     break;
                 }
                 case 'x':
                 {
-                    unsigned int num = my_va_arg<unsigned int>(vlist);
-                    vlist+= sizeof(unsigned int);
-//                    unsigned int num = va_arg(vlist, int);
+                    unsigned int num = va_arg(vlist, int);
                     itoa(p_buf, num, 16);
                     p_buf += strlen(p_buf);             //同理
                     break;
                 }
                 case 'X':
                 {
-                    unsigned int num = my_va_arg<unsigned int>(vlist);
-                    vlist+= sizeof(unsigned int);
-//                    unsigned int num = va_arg(vlist, int);
+                    unsigned int num = va_arg(vlist, int);
                     itoa(p_buf, num, 16, true);
                     p_buf += strlen(p_buf);             //同理
                     break;
                 }
                 case 'u':
                 {
-                    unsigned int num = my_va_arg<unsigned int>(vlist);
-                    vlist+= sizeof(unsigned int);
-//                    unsigned int num = va_arg(vlist, int);
+                    unsigned int num = va_arg(vlist, int);
                     itoa(p_buf, num, 10);
                     p_buf += strlen(p_buf);             //同理
                     break;
@@ -149,12 +108,12 @@ int debug_vsprintf( char* buffer, const char* format, va_list vlist )
     return p_buf - buffer;
 }
 
-int debug_sprintf( char *buffer, const char *format, ... )
+static int sprintf( char *buffer, const char *format, ... )
 {
     int ret;
     va_list va;
     va_start(va, format);
-    ret = debug_vsprintf(buffer, format, va);
+    ret = vsprintf(buffer, format, va);
     va_end(va);
     return ret;
 }
@@ -165,9 +124,9 @@ int debug_printf( const char* format, ... )
     int ret;
     va_list va;
     va_start(va, format);
-    ret = debug_vsprintf(printbuf, format, va);
+    ret = vsprintf(printbuf, format, va);
     va_end(va);
-    debug_puts(printbuf);
+    puts(printbuf);
     return ret;
 }
 static int tty_putchar(tty& dbg_tty, int ch )
@@ -190,7 +149,7 @@ int tty_debug_printf(tty& dbg_tty, const char* format, ... )
     int ret;
     va_list va;
     va_start(va, format);
-    ret = debug_vsprintf(printbuf, format, va);
+    ret = vsprintf(printbuf, format, va);
     va_end(va);
     tty_puts(dbg_tty, printbuf);
     return ret;
